4c9e19b55c16d0dfe72dacebd222da17647b1b48
Boris Capitanu <capitanu@illinois.edu>
2019-12-13 15:54:57 -0600

    Updated to include all missing JSON-LD attributes

--- src/main/scala/org/hathitrust/htrc/tools/featureextractor/features/EF.scala
+++ src/main/scala/org/hathitrust/htrc/tools/featureextractor/features/EF.scala
@@ -1,4 +1,42 @@
 package org.hathitrust.htrc.tools.featureextractor.features
 
-case class EF(id: String,
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+import play.api.libs.json.{JsObject, Json}
+
+object EF {
+  private val `@context`: String = "https://worksets.htrc.illinois.edu/context/ef_context.jsonld"
+  private val schemaVersion: String = "https://schemas.hathitrust.org/EF_Schema_v_3.0"
+  private val `type`: String = "DataFeed"
+  private val publisher: JsObject = Json.obj(
+    "id" -> "https://analytics.hathitrust.org",
+    "type" -> "Organization",
+    "name" -> "HathiTrust Research Center"
+  )
+  private val baseIdFormat: String = "https://data.analytics.hathitrust.org/extracted-features/%s/%s"
+
+  def apply(htid: String, features: VolumeFeatures): EF = {
+    val dateTimeFormatter = DateTimeFormat forPattern "yyyy-MM-dd"
+    val datePublished = DateTime.now().toString(dateTimeFormatter)
+
+    new EF(
+      `@context` = `@context`,
+      schemaVersion = schemaVersion,
+      id = baseIdFormat.format(datePublished, htid),
+      htid = htid,
+      `type` = `type`,
+      publisher = publisher,
+      datePublished = datePublished,
+      features = features
+    )
+  }
+}
+
+case class EF(`@context`: String,
+              schemaVersion: String,
+              id: String,
+              htid: String,
+              `type`: String,
+              publisher: JsObject,
+              datePublished: String,
               features: VolumeFeatures)
--- src/main/scala/org/hathitrust/htrc/tools/featureextractor/features/VolumeFeatures.scala
+++ src/main/scala/org/hathitrust/htrc/tools/featureextractor/features/VolumeFeatures.scala
@@ -1,25 +1,44 @@
 package org.hathitrust.htrc.tools.featureextractor.features
 
+import java.util.Objects
+
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
+import play.api.libs.json.{JsObject, Json}
 
 object VolumeFeatures {
-  val schemaVersion: String = "4.0"
+  private val `type`: String = "DataFeedItem"
+  private val handleUrlBase: String = "http://hdl.handle.net/2027/"
+  private val schemaVersion: String = "https://schemas.hathitrust.org/EF_Schema_FeaturesSubSchema_v_3.0"
+  private val creator: JsObject = Json.obj(
+    "id" -> "https://analytics.hathitrust.org",
+    "type" -> "Organization",
+    "name" -> "HathiTrust Research Center"
+  )
 
-  def apply(pages: Seq[HtrcPageFeatures]): VolumeFeatures = {
+  def apply(volId: String, pages: Seq[HtrcPageFeatures]): VolumeFeatures = {
+    require(Objects.nonNull(volId), "volId cannot be null")
+    require(Objects.nonNull(pages), "pages cannot be null")
+
     val dateTimeFormatter = DateTimeFormat forPattern "yyyy-MM-dd'T'HH:mm"
     val dateCreated = DateTime.now().toString(dateTimeFormatter)
 
     new VolumeFeatures(
+      `type` = `type`,
+      id = handleUrlBase + volId,
       schemaVersion = schemaVersion,
       dateCreated = dateCreated,
+      creator = creator,
       pageCount = pages.size,
       pages = pages
     )
   }
 }
 
-case class VolumeFeatures(schemaVersion: String,
+case class VolumeFeatures(`type`: String,
+                          id: String,
+                          schemaVersion: String,
                           dateCreated: String,
+                          creator: JsObject,
                           pageCount: Int,
                           pages: Seq[HtrcPageFeatures])
