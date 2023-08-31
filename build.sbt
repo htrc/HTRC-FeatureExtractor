47dd6147057cb43aa650eb031499b6addbad4bff
Boris Capitanu <capitanu@illinois.edu>
2023-08-31 11:12:05 -0500

    Minor fixes

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-import Dependencies._
+import Dependencies.*
 
 showCurrentGitBranch
 
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,19 +1,13 @@
-import sbt.Keys._
-import sbt._
+import sbt.Keys.*
+import sbt.*
 
 object Dependencies {
 
   def spark(version: String): Seq[Def.Setting[Seq[ModuleID]]] = Seq(
     libraryDependencies ++= Seq(
-      "org.apache.spark" %% "spark-core"      % version % Provided
-        exclude("org.slf4j", "slf4j-log4j12")
-        exclude("org.apache.logging.log4j", "log4j-slf4j2-impl"),
-      "org.apache.spark" %% "spark-streaming" % version % Provided
-        exclude("org.slf4j", "slf4j-log4j12")
-        exclude("org.apache.logging.log4j", "log4j-slf4j2-impl"),
+      "org.apache.spark" %% "spark-core"      % version % Provided,
+      "org.apache.spark" %% "spark-streaming" % version % Provided,
       "org.apache.spark" %% "spark-sql"       % version % Provided
-        exclude("org.slf4j", "slf4j-log4j12")
-        exclude("org.apache.logging.log4j", "log4j-slf4j2-impl")
     )
   )
 
